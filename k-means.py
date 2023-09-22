76e2f852303af587722d00353d06ac9e75309465
Guido Lorenzetti <guidobruera@gmail.com>
2023-09-22 19:30:54 -0300

    k-means start

---
+++ k-means.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sklearn.cluster as cluster
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+
+# Read the data
+data = pd.read_csv('Crop_recommendation_corrected.csv')
+
+
