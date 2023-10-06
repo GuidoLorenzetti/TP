351c2ff80ccbe4f8c775e213ec27dfd7ec36cc44
augustofarias2 <augustofarias027@gmail.com>
2023-10-06 16:10:51 -0300

    Actualizo outliers, t-sne e isomap

--- isomap.py
+++ isomap.py
@@ -2,69 +2,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
-import seaborn as sns
-import sklearn as sk
-import seaborn as sns
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import Isomap
 
+# Cargar el dataset
 df = pd.read_csv('Crop_recommendation.csv')
 
-# Utiliza seaborn para generar una paleta de colores única basada en los valores únicos en la columna 'Datos'
-palette = sns.color_palette('husl', n_colors=len(df['label'].unique()))
+# Convierte la columna 'label' en una columna categórica
+df['label'] = df['label'].astype('category')
 
-# Crea un diccionario que asigne valores únicos a colores de la paleta
-color_dict = dict(zip(df['label'].unique(), palette))
+# Selecciona las características numéricas
+xWheat = df[['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']]
 
-# Aplica el mapeo de colores a la columna 'Datos' para crear la nueva columna 'Colores'
-df['Colores'] = df['label'].map(color_dict)
-
-df['label']=df['label'].astype('category')
-
-
-#info df
-#df.info()
-#df.describe()
-#df.isnull().sum()
-
-xWheat = df.drop(['label'], axis=1).drop(['Colores'], axis=1)
-yWheat = df['label']
+# Escala las características
 xWheat_scaled = StandardScaler().fit_transform(xWheat)
 
-xWS_df=pd.DataFrame(xWheat_scaled)
-xWS_df.columns=['N','P','K','temperature','humidity','ph','rainfall']
-xWS_df['label']=yWheat
-xWS_df['Colores']=df['Colores']
-
-# xWheat = df[['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']]
-# yWheat = df['label']
-
-#xWheat_scaled = sk.preprocessing.StandardScaler().fit_transform(xWheat)
-
-####2D########
-# plt.figure(figsize=(8, 6))  # Ajusta el tamaño del gráfico
-# isomapWheat = Isomap(n_neighbors=20, n_components=2)
-# isomapWheat.fit(xWheat_scaled)
-# manifold_2Da = isomapWheat.transform(xWheat_scaled)
-# manifold_2D = pd.DataFrame(manifold_2Da, columns=['Component 1', 'Component 2'])
-# manifold_2D['category'] = yWheat.to_numpy()
-
-# groups = manifold_2D.groupby('category')
-# plt.title('2D Isomap Graph')
-# for name, group in groups:
-#     plt.plot(group['Component 1'], group['Component 2'], marker='o', linestyle='', markersize=5, label=name)
-# plt.legend()
-# plt.show()
-
-####3D########
+# Realiza la reducción de dimensionalidad utilizando Isomap en 3D
 isomapWheat = Isomap(n_neighbors=35, n_components=3)
-isomapWheat.fit(xWheat_scaled)
-manifold_3Da = isomapWheat.transform(xWheat_scaled)
-manifold_3D = pd.DataFrame(manifold_3Da,
-    columns=['Component 1', 'Component 2','Component 3'])
-manifold_3D['category'] = yWheat.to_numpy()
-fig = px.scatter_3d(manifold_3D, x='Component 1', y='Component 2', z='Component 3', color='category',
+manifold_3D = isomapWheat.fit_transform(xWheat_scaled)
+
+# Crea un DataFrame con las componentes principales y la columna 'label'
+df_3D = pd.DataFrame(manifold_3D, columns=['Component 1', 'Component 2', 'Component 3'])
+df_3D['label'] = df['label']
+
+# Crea una visualización 3D utilizando Plotly Express
+fig = px.scatter_3d(df_3D, x='Component 1', y='Component 2', z='Component 3', color='label',
                     title='3D Isomap Graph')
 fig.show()
--- t-sne.py
+++ t-sne.py
@@ -1,61 +1,38 @@
-from sklearn.manifold import TSNE
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
 import seaborn as sns
-import sklearn as sk
-import seaborn as sns
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
-from sklearn.manifold import Isomap
+from sklearn.manifold import TSNE
 
-""" t-SNE """
+# Cargar el conjunto de datos desde un archivo CSV
 df = pd.read_csv('Crop_recommendation.csv')
 
-# Utiliza seaborn para generar una paleta de colores única basada en los valores únicos en la columna 'Datos'
+# Generar una paleta de colores única basada en las etiquetas de clase
 palette = sns.color_palette('husl', n_colors=len(df['label'].unique()))
 
-# Crea un diccionario que asigne valores únicos a colores de la paleta
+# Crear un diccionario para mapear etiquetas de clase a colores
 color_dict = dict(zip(df['label'].unique(), palette))
 
-# Aplica el mapeo de colores a la columna 'Datos' para crear la nueva columna 'Colores'
+# Aplicar el mapeo de colores a la columna 'label' y crear la columna 'Colores'
 df['Colores'] = df['label'].map(color_dict)
 
-df['label']=df['label'].astype('category')
+# Convertir la columna 'label' en un tipo categórico
+df['label'] = df['label'].astype('category')
 
+# Seleccionar características numéricas y escalarlas
+features = df.drop(['label', 'Colores'], axis=1)
+scaled_features = StandardScaler().fit_transform(features)
 
-#info df
-#df.info()
-#df.describe()
-#df.isnull().sum()
+# Aplicar t-SNE para reducir la dimensionalidad a 2D
+tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+tsne_results = tsne.fit_transform(scaled_features)
 
-xWheat = df.drop(['label'], axis=1).drop(['Colores'], axis=1)
-yWheat = df['label']
-xWheat_scaled = StandardScaler().fit_transform(xWheat)
+# Crear un DataFrame con etiquetas originales y resultados de t-SNE
+tsne_df = pd.DataFrame({'label': df['label'], 'tsne-2d-one': tsne_results[:, 0], 'tsne-2d-two': tsne_results[:, 1]})
 
-xWS_df=pd.DataFrame(xWheat_scaled)
-xWS_df.columns=['N','P','K','temperature','humidity','ph','rainfall']
-xWS_df['label']=yWheat
-xWS_df['Colores']=df['Colores']
-
-# xWheat = df[['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']]
-# yWheat = df['label']
-
-#xWheat_scaled = sk.preprocessing.StandardScaler().fit_transform(xWheat)
-
-tsneWheat = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-tsneWheatResults = tsneWheat.fit_transform(xWheat_scaled)
-
-subsetWheatTSNE = pd.DataFrame(yWheat)
-subsetWheatTSNE['tsne-2d-one'] = tsneWheatResults[:,0]
-subsetWheatTSNE['tsne-2d-two'] = tsneWheatResults[:,1]
-plt.figure(figsize=(16,10))
-sns.scatterplot(
-    x="tsne-2d-one", y="tsne-2d-two", hue="label",
-    palette=sns.color_palette("hls", 22), 
-    data=subsetWheatTSNE,
-    legend="full", alpha=0.8)
-
+# Crear un gráfico de dispersión de 2D con colores basados en las etiquetas de clase
+plt.figure(figsize=(12, 8))
+sns.scatterplot(x="tsne-2d-one", y="tsne-2d-two", hue="label", palette=palette, data=tsne_df, legend="full", alpha=0.8)
+plt.title("t-SNE Visualization")
 plt.show()
